174be7ef55a3ff3c1ac5d5259f2c6b60f721afec
Verta-master <vertyagina@gmail.com>
2020-05-31 16:21:09 +0600

    Update 2020-05-31T10:21:04.650Z

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,22 @@
 // Range slider
 $( function() {
+  var handle1 = $( "#handle-min" );
+  var handle2 = $( "#handle-max" );
+  
   $( "#slider-range" ).slider({
     range: true,
     min: 0,
     max: 150,
     values: [ 35, 98 ],
+    create: function() {
+      handle1.text( $( this ).slider( "values", 0 ) );
+      handle2.text( $( this ).slider( "values", 1 ) );
+    },
     slide: function( event, ui ) {
       $( "#amount-min" ).val( ui.values[ 0 ] );
       $( "#amount-max" ).val( ui.values[ 1 ] );
+      handle1.text( ui.values[ 0 ] );
+      handle2.text( ui.values[ 1 ] );
     }
   });
-//  $( "#amount-min" ).val( $( "#slider-range" ).slider( "values", 0 ) );
-//  $( "#amount-max" ).val( $( "#slider-range" ).slider( "values", 1 ) );
 });
